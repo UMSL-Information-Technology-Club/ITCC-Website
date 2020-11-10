88a3e5d652e03495813b3be767cf869e64bc9732
Alex Helton <heltonat@umsystem.edu>
2020-11-09 19:11:43 -0600

    made links clickable in OurProjects

--- main.js
+++ main.js
@@ -12,15 +12,24 @@ request.onload = function () {
 
     /*loop through the specified JSON properties, retrieve data, 
     and format it into the bootstrap table */
+
+    /* cash money */
     $.each(data, function(i, status) {
         statusHTML += '<tr>';
         statusHTML += '<td>' + status.id + '</td>';
         statusHTML += '<td>' + status.name + '</td>';
         statusHTML += '<td>' + status.description + '</td>';
+        statusHTML += '<td id="statusHtml">' + '<a href= '+ status.html_url + '>' + status.html_url + '</td>';
+
+        /* so for reference this mess is
         statusHTML += '<td>' + status.html_url + '</td>';
+        but there's no decent way to wrench a link in there...
+        */
+
         statusHTML += '<td>' + status.language + '</td>';
         statusHTML += '</tr>';
     });
+
 
     //Display data on page
     $('tbody').html(statusHTML);
